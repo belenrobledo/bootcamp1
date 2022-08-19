48e08d797bd63832c8e5cb9c2de34bfd274b2ed1
belenrobledo <111546680+belenrobledo@users.noreply.github.com>
2022-08-18 22:04:24 -0300

    Add files via upload

---
+++ index.java
@@ -0,0 +1,24 @@
+public class main {
+    int resultado;
+    resultado = suma(10,10,10);
+
+public class miCoche {
+
+    sumarPuertas();
+    System.out.println(puertas);
+}
+
+
+}
+public static int suma (int a, int b, int c) {
+    return a + b + c;
+}
+
+public class Coche{
+    int puertas=3;
+}
+public int sumarPuertas() {
+    for(int i=3; i<=5; i++){
+        puertas = puertas + 1;
+    }
+}
